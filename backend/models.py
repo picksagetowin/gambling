f8b7d9d03376e1ff30b596f798fd6e01f6e6e3c0
s1tgxy_10 <2010donghyeoklee@gmail.com>
2026-08-30 14:04:13 +0900

    fix:깃레포및로컬 꼬인거 해결,feat:schemas.py(basemodel)구축

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, String, Boolean, DateTime
 from sqlalchemy import func
 from sqlalchemy.sql.schema import Column
 from .database import Base
@@ -12,5 +12,6 @@ class Users(Base):
     tag = Column(String(5), nullable=True)
     password = Column(String(255), nullable=False)
     puuid = Column(String, unique=True, nullable=False)
+    is_ative = Column(Boolean, nullable=False, defualt=False)
     money = Column(Integer, nullable=False, default=0)
     created_at = Column(DateTime, default=func.now())

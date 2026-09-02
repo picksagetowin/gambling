3ed95a8a115970d5fa4105627b0f558e46119700
s1tgxy_10 <2010donghyeoklee@gmail.com>
2026-09-03 00:04:24 +0900

    feat:공지사항 배너추가,fix:배경색 변경

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -21,6 +21,21 @@ function Homepage(){
                 </div>
             </div>
 
+            <div className='info'>
+                <div className='notice-container'>
+                    <div className='notice-banner'>
+                        <div className='notice-left'>
+                            {/* <svg className='notice-icon' viewBox='0 0 24 24' 
+                            fill='none' stroke='currentColor' strokeWidth='2'>
+                                <path d='m3 11 18-5v12L3 14v-3z' />
+                                <path d='M11.6 16.8a3 3 0 1 1-5.8-1.6' />
+                            </svg> */}
+                            <span className='notice-word'>공지사항</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }

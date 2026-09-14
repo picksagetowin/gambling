fc771eb03a32127cd8055944138da9e958085c35
s1tgxy_10 <2010donghyeoklee@gmail.com>
2026-09-14 14:45:49 +0900

    feat:시상대 추가 및 준비중 아이콘 추가

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -36,57 +36,93 @@ function Homepage(){
                         </div>
                     </div>
                 </div>
+            </section>
                 
                 <section className='dashboard'>
-                    <div className='games'>
-                        <div className='game-banner'>
-                            <div className='banner-header'>
-                                <h3>
-                                    <span className='games-icon'>❓</span>
-                                    인기 게임
-                                </h3>
-                            </div>
-                            <div className='game-cards'>
-                                <div className='game-card'>
-                                    <div className='game-img'>
-                                        <img src='maingame.png' alt='게임사진' />
-                                    </div>
-                                    <div className='game-details'>
-                                        <h4>확률게임</h4>
-                                    </div>
+                    <div className='dashboard-container'>
+                        <div className='games'>
+                            <div className='banner'>
+                                <div className='banner-header'>
+                                    <h3>
+                                        <span className='games-icon'>❓</span>
+                                        인기 게임
+                                    </h3>
                                 </div>
+                                <div className='game-cards'>
+                                    <div className='game-card'>
+                                        <div className='game-img'>
+                                            <img src='maingame.png' alt='게임사진' />
+                                        </div>
+                                        <div className='game-details'>
+                                            <h4>확률게임</h4>
+                                        </div>
+                                    </div>
 
-                                <div className='game-card'>
-                                    <div className='game-img'>
-                                        <img src='' alt='게임사진' />
+                                    <div className='game-card'>
+                                        <div className='game-img'>
+                                            <img src='ready.png' alt='게임사진' />
+                                        </div>
+                                        <div className='game-details'>
+                                            <h4>게임 준비중</h4>
+                                        </div>
                                     </div>
-                                    <div className='game-details'>
-                                        <h4>게임 준비중</h4>
-                                    </div>
-                                </div>
 
-                                <div className='game-card'>
-                                    <div className='game-img'>
-                                        <img src='' alt='게임사진' />
+                                    <div className='game-card'>
+                                        <div className='game-img'>
+                                            <img src='ready.png' alt='게임사진' />
+                                        </div>
+                                        <div className='game-details'>
+                                            <h4>게임 준비중</h4>
+                                        </div>
                                     </div>
-                                    <div className='game-details'>
-                                        <h4>게임 준비중</h4>
-                                    </div>
-                                </div>
-                                <div className='game-card'>
-                                    <div className='game-img'>
-                                        <img src='' alt='게임사진' />
-                                    </div>
-                                    <div className='game-details'>
-                                        <h4>게임 준비중</h4>
-                                    </div>
-                                </div>
+                                    {/* <div className='game-card'>
+                                        <div className='game-img'>
+                                            <img src='' alt='게임사진' />
+                                        </div>
+                                        <div className='game-details'>
+                                            <h4>게임 준비중</h4>
+                                        </div>
+                                    </div> */}
                                 
+                                </div>
+                            </div>
+                        </div>
+                        <div className='ranking'>
+                            <div className='banner'>
+                                <div className='banner-header'>
+                                    <h3>
+                                        <span className='ranking-icon' aria-hidden='true'>🏆</span>
+                                        실시간 랭킹
+                                    </h3>
+                                    <span className='ranking-period'>이번 라운드</span>
+                                </div>
+ 
+                                <div className='top-players' role='list' aria-label='실시간 랭킹 상위 3명'>
+                                    <div className='player first-player' role='listitem' aria-label='1위 USER 01, 1,280 포인트'>
+                                        <div className='medal' aria-hidden='true'>🥇</div>
+                                        <span className='player-name'>USER 01</span>
+                                        <span className='player-money'>1,280 P</span>
+                                        <div className='podium first-place' aria-hidden='true'>1</div>
+                                    </div>
+                                    <div className='player second-player' role='listitem' aria-label='2위 USER 02, 860 포인트'>
+                                        <div className='medal' aria-hidden='true'>🥈</div>
+                                        <span className='player-name'>USER 02</span>
+                                        <span className='player-money'>860 P</span>
+                                        <div className='podium second-place' aria-hidden='true'>2</div>
+                                    </div>
+                                    <div className='player third-player' role='listitem' aria-label='3위 USER 03, 520 포인트'>
+                                        <div className='medal' aria-hidden='true'>🥉</div>
+                                        <span className='player-name'>USER 03</span>
+                                        <span className='player-money'>520 P</span>
+                                        <div className='podium third-place' aria-hidden='true'>3</div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </section>
-            </section>
+            
         </main>
     )
 }
